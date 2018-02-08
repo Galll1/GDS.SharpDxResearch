4ff80c7fffbadefa2bdacdea49c438ee78d33306
Galll1 <galll.heken@gmail.com>
2018-02-08 21:06:48 +0100

    Initial

---
+++ GDS.SharpDxResearch.Boot/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace GDS.SharpDxResearch.Boot
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
