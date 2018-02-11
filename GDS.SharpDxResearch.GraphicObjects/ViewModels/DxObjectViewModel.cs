f39825b46aad4ace250d22d9f274efebc2c4b80f
Galll1 <galll1.heken@gmail.com>
2018-02-11 21:43:09 +0100

    GDS-2 Try to resolve object mesh by vm

---
+++ GDS.SharpDxResearch.GraphicObjects/ViewModels/DxObjectViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Windows.Media;
+using System.Windows.Media.Media3D;
+
+namespace GDS.SharpDxResearch.GraphicObjects.ViewModels
+{
+    public class DxObjectViewModel
+    {
+        public Point3DCollection Positions { get; set; }
+        public Int32Collection TriangleIndices { get; set; }
+
+        public DxObjectViewModel()
+        {
+            Positions = new Point3DCollection(GetObjectPositionPoints());
+            TriangleIndices = new Int32Collection(GetObjectTriangleIndices());
+        }
+
+        private IEnumerable<int> GetObjectTriangleIndices()
+        {
+            yield return 2; yield return 3; yield return 1;
+            yield return 2; yield return 1; yield return 0;
+            yield return 7; yield return 1; yield return 3;
+            yield return 7; yield return 5; yield return 1;
+            yield return 6; yield return 5; yield return 7;
+            yield return 6; yield return 4; yield return 5;
+            yield return 6; yield return 2; yield return 0;
+            yield return 2; yield return 0; yield return 4;
+            yield return 2; yield return 7; yield return 3;
+            yield return 2; yield return 6; yield return 7;
+            yield return 0; yield return 1; yield return 5;
+            yield return 0; yield return 5; yield return 4;
+        }
+
+        private IEnumerable<Point3D> GetObjectPositionPoints()
+        {
+            yield return new Point3D(0, 0, 0);
+            yield return new Point3D(1, 0, 0);
+            yield return new Point3D(0, 1, 0);
+            yield return new Point3D(1, 1, 0);
+            yield return new Point3D(0, 0, 1);
+            yield return new Point3D(1, 0, 1);
+            yield return new Point3D(0, 1, 1);
+            yield return new Point3D(1, 1, 0);
+        }
+    }
+}
---
+++ GDS.SharpDxResearch.SharpDx.MainModule/ViewModels/RenderViewModel.cs
@@ -0,0 +1,14 @@
+﻿using GDS.SharpDxResearch.GraphicObjects.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GDS.SharpDxResearch.SharpDx.MainModule.ViewModels
+{
+    public class RenderViewModel
+    {
+        public DxObjectViewModel Geometry => new DxObjectViewModel();
+    }
+}
